64a93c35de22c5a1225940d5b582a15bff5dd5d4
lbong2 <dkw05121@konkuk.ac.kr>
2023-04-12 20:21:55 +0000

    feat: "baseline code 기능 추가"
    model.py timm model class 추가

---
+++ kyungbong/baseline_v1/model.py
@@ -0,0 +1,44 @@
+import torch.nn as nn
+import torch 
+import torch.nn.functional as F
+import timm 
+
+
+
+class BaseModel(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=7, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
+        self.dropout1 = nn.Dropout(0.25)
+        self.dropout2 = nn.Dropout(0.25)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout2(x)
+
+        x = self.avgpool(x)
+        x = x.view(-1, 128)
+        return self.fc(x)
+
+class timmModel(nn.Module):
+    def __init__(self, num_classes):
+        super(timmModel, self).__init__()
+        self.model = timm.create_model('resnet50', pretrained=True, num_classes=num_classes)
+
+    def forward(self, x):
+        return self.model(x)
