53b45423bbb2b68471f38ccc7526e36e3cbe2030
lbong2 <dkw05121@konkuk.ac.kr>
2023-04-13 00:17:03 +0000

    feat: "baseline code update"
    load_model 코드 model.py의 build_model과 호환이 맞지 않아 수정
    --resize default value 수정

--- kyungbong/baseline_v1/inference.py
+++ kyungbong/baseline_v1/inference.py
@@ -11,16 +11,17 @@ from dataset import TestDataset, MaskBaseDataset
 
 
 def load_model(saved_model, num_classes, device):
-    model_cls = getattr(import_module("model"), args.model)
-    model = model_cls(
-        num_classes=num_classes
+    model_func = getattr(import_module("model"), args.model)
+    model = model_func(
+        num_classes=num_classes, device=device
     )
 
     # tarpath = os.path.join(saved_model, 'best.tar.gz')
     # tar = tarfile.open(tarpath, 'r:gz')
     # tar.extractall(path=saved_model)
 
-    model_path = os.path.join(saved_model, 'best.pth')
+    #model_path = os.path.join(saved_model, 'best.pth')
+    model_path = saved_model
     model.load_state_dict(torch.load(model_path, map_location=device))
 
     return model
@@ -72,7 +73,7 @@ if __name__ == '__main__':
 
     # Data and model checkpoints directories
     parser.add_argument('--batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
-    parser.add_argument('--resize', type=tuple, default=(96, 128), help='resize size for image when you trained (default: (96, 128))')
+    parser.add_argument('--resize', type=tuple, default=[128, 96], help='resize size for image when you trained (default: [128, 96])')
     parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
 
     # Container environment
