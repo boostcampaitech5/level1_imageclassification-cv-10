1eb5d740da0065a4add9b1a5d56203d8b3b1695b
lbong2 <dkw05121@konkuk.ac.kr>
2023-04-17 06:06:07 +0000

    Feat: "Ensemble 관련 코드 추가"

---
+++ train_cv_v2.py
@@ -0,0 +1,387 @@
+import argparse
+import glob
+import json
+import multiprocessing
+import os
+import random
+import re
+from importlib import import_module
+from pathlib import Path
+import copy
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.optim import SGD, Adam
+from torch.optim.lr_scheduler import StepLR
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from dataset import MaskBaseDataset
+from loss import create_criterion
+
+import wandb
+
+from sklearn.model_selection import StratifiedGroupKFold,StratifiedKFold,KFold,GroupKFold
+
+# -- task
+task_dict = {'default': 18, 'mask': 3, 'gender': 2, 'age': 3}
+
+def seed_everything(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+def grid_image(np_images, gts, preds, n=16, shuffle=False):
+    batch_size = np_images.shape[0]
+    assert n <= batch_size
+
+    choices = random.choices(range(batch_size), k=n) if shuffle else list(range(n))
+    figure = plt.figure(figsize=(12, 18 + 2))  # cautions: hardcoded, 이미지 크기에 따라 figsize 를 조정해야 할 수 있습니다. T.T
+    plt.subplots_adjust(top=0.8)  # cautions: hardcoded, 이미지 크기에 따라 top 를 조정해야 할 수 있습니다. T.T
+    n_grid = int(np.ceil(n ** 0.5))
+    tasks = ["mask", "gender", "age"]
+    for idx, choice in enumerate(choices):
+        gt = gts[choice].item()
+        pred = preds[choice].item()
+        image = np_images[choice]
+        gt_decoded_labels = MaskBaseDataset.decode_multi_class(gt)
+        pred_decoded_labels = MaskBaseDataset.decode_multi_class(pred)
+        title = "\n".join([
+            f"{task} - gt: {gt_label}, pred: {pred_label}"
+            for gt_label, pred_label, task
+            in zip(gt_decoded_labels, pred_decoded_labels, tasks)
+        ])
+
+        plt.subplot(n_grid, n_grid, idx + 1, title=title)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(image, cmap=plt.cm.binary)
+
+    return figure
+
+
+def increment_path(path, exist_ok=False):
+    """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+
+    Args:
+        path (str or pathlib.Path): f"{model_dir}/{args.name}".
+        exist_ok (bool): whether increment path (increment if False).
+    """
+    path = Path(path)
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}*")
+        matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]
+        n = max(i) + 1 if i else 2
+        return f"{path}{n}"
+
+def getDataloader(dataset, train_idx, valid_idx, batch_size, num_workers):
+    # 인자로 전달받은 dataset에서 train_idx에 해당하는 Subset 추출
+    if args.cross_val_n_splits!=0:
+        train_set = torch.utils.data.Subset(dataset,
+                                            indices=train_idx)
+        # 인자로 전달받은 dataset에서 valid_idx에 해당하는 Subset 추출
+        val_set   = torch.utils.data.Subset(dataset,
+                                            indices=valid_idx)
+    
+    # cv를 안쓸경우 이미 set으로 만들어져서 전달된다.
+    else:
+        train_set = train_idx
+        val_set = valid_idx
+    val_len = len(val_set)
+        
+    # 추출된 Train Subset으로 DataLoader 생성
+    train_loader = torch.utils.data.DataLoader(
+        train_set,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        drop_last=True,
+        shuffle=True
+    )
+    # 추출된 Valid Subset으로 DataLoader 생성
+    val_loader = torch.utils.data.DataLoader(
+        val_set,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        drop_last=True,
+        shuffle=False
+    )
+    
+    # 생성한 DataLoader 반환
+    return train_loader, val_loader, val_len
+
+def train(data_dir, model_dir, args):
+    seed_everything(args.seed)
+
+    save_dir = increment_path(os.path.join('./' + model_dir, args.name))
+
+    # -- settings
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    # -- dataset
+    dataset_module = getattr(import_module("dataset"), args.dataset)  # default: MaskSplitByProfileDataset
+    dataset = dataset_module(
+        data_dir=data_dir,
+    )
+    
+    num_classes = task_dict[args.task]
+
+    # -- augmentation
+    transform_module = getattr(import_module("dataset"), args.augmentation)  # default: AlbumAugmentation
+    transform = transform_module(
+        resize=args.resize,
+        mean=dataset.mean,
+        std=dataset.std,
+    )
+    dataset.set_transform(transform)
+##########
+    # cross-validation setup
+    n_splits=int(args.cross_val_n_splits)
+    counter = 0
+    patience = 10
+    accumulation_steps = args.accumulation
+    best_val_acc = 0
+    best_val_loss = np.inf
+    val_loss_result = defaultdict(list)
+    val_acc_result = defaultdict(list)
+    if args.cross_val_n_splits >=3:
+        cv = None
+        
+        labels = [dataset.encode_multi_class(mask, gender, age) for mask, gender, age in zip(dataset.mask_labels, dataset.gender_labels, dataset.age_labels)]
+
+        if args.cross_val_type == 'StratifiedGroup':
+            cv = StratifiedGroupKFold(n_splits=n_splits).split(dataset.image_paths,labels,dataset.group)
+        elif args.cross_val_type == 'Stratified':
+            cv = StratifiedKFold(n_splits=n_splits).split(dataset.image_paths,labels)
+        elif args.cross_val_type == 'Group':
+            cv = GroupKFold(n_splits=n_splits).split(dataset.image_paths,labels,dataset.group)
+        elif args.cross_val_type == 'Normal':
+            cv = KFold(n_splits=n_splits).split(dataset.image_paths,labels)
+        else:
+            raise("wrong cross val type")
+
+
+    else:
+        cv = []
+        cv.append(dataset.split_dataset())
+        
+    for i, (train_idx, valid_idx) in enumerate(cv):
+        train_loader,val_loader,val_len = getDataloader(dataset,train_idx,valid_idx,args.batch_size,multiprocessing.cpu_count() // 2)
+##########
+        # -- model
+        # model = BaseModel(num_classes=num_classes).to(device)
+        model_module = getattr(import_module("model"), args.model + "_Model")
+        model = model_module(num_classes=num_classes, lr=args.lr).to(device)
+        # model = torch.nn.DataParallel(model)
+
+        # -- loss & metric
+        criterion = create_criterion(args.criterion) 
+        if args.criterion == 'f1' or args.criterion == 'label_smoothing':
+            criterion.classes = task_dict[args.task]
+
+        opt_module = getattr(import_module("torch.optim"), args.optimizer)  # default: Adam
+
+        '''train_params = [{'params': getattr(model, 'features').parameters(), 'lr': lr / 10, 'weight_decay':5e-4},
+                    {'params': getattr(model, 'classifier').parameters(), 'lr': lr, 'weight_decay':5e-4}]'''
+            
+        optimizer = opt_module(
+            model.train_params
+        )
+        scheduler = StepLR(optimizer, args.lr_decay_step, gamma=0.5)
+
+        ## 수정금지 ##
+        logger = SummaryWriter(log_dir=save_dir)
+        with open(os.path.join(save_dir, 'config.json'), 'w', encoding='utf-8') as f:
+            json.dump(vars(args), f, ensure_ascii=False, indent=4)
+        ## 수정금지 ##
+
+
+        # getattr로 decode 함수 가져와서 적용 
+        decode_func = getattr(import_module("dataset"), f"decode_{args.task}_class" )
+        for epoch in range(args.epochs):
+            # train loop
+            model.train()
+            loss_value = 0
+            matches = 0
+            for idx, train_batch in enumerate(train_loader):
+                inputs, labels = train_batch
+
+                labels = decode_func(labels)
+
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                outs = model(inputs)
+                preds = torch.argmax(outs, dim=-1)
+                loss = criterion(outs, labels)
+
+                loss.backward()
+                if (idx+1) % accumulation_steps == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
+
+                loss_value += loss.item()
+                matches += (preds == labels).sum().item()
+                if (idx + 1) % args.log_interval == 0:
+                    train_loss = loss_value / args.log_interval
+                    train_acc = matches / args.batch_size / args.log_interval
+                    current_lr = get_lr(optimizer)
+                    print(
+                        f"{i+1}번째 cv: Epoch[{epoch+1}/{args.epochs}]({idx + 1}/{len(train_loader)}) ||\t"
+                        f"training {args.criterion} loss {train_loss:4.4} ||\ttraining accuracy {train_acc:4.2%} ||\tlr {current_lr}"
+                    )
+                    
+                    if args.wdb_on:
+                        wandb.log({
+                            f"Train/{args.criterion} loss": train_loss, "Train/accuracy": train_acc})
+                    
+                    loss_value = 0
+                    matches = 0
+
+            scheduler.step()
+
+            # val loop
+            with torch.no_grad():
+                print("Calculating validation results...")
+                model.eval()
+                val_loss_items = []
+                val_acc_items = []
+                figure = None
+
+                for val_batch in val_loader:
+                    inputs, labels = val_batch
+                    labels = decode_func(labels)
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    outs = model(inputs)
+                    preds = torch.argmax(outs, dim=-1)
+
+                    loss_item = criterion(outs, labels).item()
+                    acc_item = (labels == preds).sum().item()
+                    val_loss_items.append(loss_item)
+                    val_acc_items.append(acc_item)
+
+                    if figure is None:
+                        inputs_np = torch.clone(inputs).detach().cpu().permute(0, 2, 3, 1).numpy()
+                        inputs_np = MaskBaseDataset.denormalize_image(inputs_np, dataset.mean, dataset.std)
+                        figure = grid_image(inputs_np, labels, preds, n=16, shuffle=True)
+
+                val_loss = np.sum(val_loss_items) / len(val_loader)
+                val_acc = np.sum(val_acc_items) / val_len
+                best_val_loss = min(best_val_loss, val_loss)
+                val_loss_result[f'fold_{i+1}'].append(val_loss)
+                val_acc_result[f'fold_{i+1}'].append(val_acc)
+                if val_acc > best_val_acc:
+                    print(f"New best model for val accuracy : {val_acc:4.2%}! saving the best model dict..")
+                    best_model = {
+                            "model": copy.deepcopy(model.state_dict()),
+                            "path": f"./{save_dir}/{epoch:03}_accuracy_{val_acc:4.2%}.pth"
+                        }
+                    best_val_acc = val_acc
+                    counter = 0
+                else:
+                    counter += 1
+                
+                if counter > patience:
+                    print("Early Stopping...")
+                    break
+                
+                print(
+                    f"[Val] acc : {val_acc:4.2%}, {args.criterion} loss: {val_loss:4.2} ||\t"
+                    f"best acc : {best_val_acc:4.2%}, best {args.criterion} loss: {best_val_loss:4.2}"
+                )
+                if args.wdb_on:
+                    wandb.log({
+                            f"Val/ {args.criterion} loss": val_loss, "Val/accuracy": val_acc
+                            })
+                
+                print()
+##########             
+    print('result of validations')
+    for key in val_acc_result.keys():
+        print(f'{key} => acc: {np.mean(val_acc_result[key]):4.2%}  ||  loss: {np.mean(val_loss_result[key]):4.2}  ')
+##########
+    torch.save(best_model['model'], best_model['path'])        
+
+
+if __name__ == '__main__':
+    torch.cuda.empty_cache()
+    parser = argparse.ArgumentParser()
+
+    # Data and model checkpoints directories
+    parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
+    parser.add_argument('--epochs', type=int, default=1, help='number of epochs to train (default: 1)')
+    parser.add_argument('--dataset', type=str, default='MaskSplitByProfileDataset', help='dataset augmentation type (default: MaskSplitByProfileDataset)')
+    parser.add_argument('--augmentation', type=str, default='BaseAugmentation', help='data augmentation type (default: BaseAugmentation)')
+    parser.add_argument("--resize", nargs="+", type=int, default=[128, 96], help='resize size for image when training')
+    parser.add_argument('--batch_size', type=int, default=64, help='input batch size for training (default: 64)')
+    parser.add_argument('--valid_batch_size', type=int, default=1000, help='input batch size for validing (default: 1000)')
+    parser.add_argument('--model', type=str, default='BaseModel', help='model type (default: BaseModel)')
+    parser.add_argument('--optimizer', type=str, default='SGD', help='optimizer type (default: SGD)')
+    parser.add_argument('--lr', type=float, default=1e-4, help='learning rate (default: 1e-4)')
+    parser.add_argument('--val_ratio', type=float, default=0.2, help='ratio for validaton (default: 0.2)')
+    parser.add_argument('--criterion', type=str, default='cross_entropy', help='criterion type (default: cross_entropy)')
+    parser.add_argument('--lr_decay_step', type=int, default=20, help='learning rate scheduler deacy step (default: 20)')
+    parser.add_argument('--log_interval', type=int, default=20, help='how many batches to wait before logging training status')
+##########
+    parser.add_argument('--cross_val_n_splits', type=int, default=0, help='number of cv fold, off if less than 3')
+    parser.add_argument('--cross_val_type', default='StratifiedGroup', help='useless if cross_val_n_splits<3, option : StratifiedGroup(default),Stratified, Group, Normal')
+    parser.add_argument('--accumulation', type=int, default=1, help='set accumulation of gradient, accumulation은 forward 몇 번에 backward 1번할지 정한다.')
+
+##########
+    parser.add_argument('--name', default='exp', help='model save at {SM_MODEL_DIR}/{name}')
+    parser.add_argument('--wdb_on', type=bool, default=False, help='turn on wandb(if you set True)')
+    parser.add_argument('--task', type=str, default='default', help='select task you want(default: default) ex: [mask, gender, age]')
+    # loss 말고 acc, f1 선택할 수 있는 기능
+
+    # Container environment
+    parser.add_argument('--data_dir', type=str, default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/input/data/train/images'))
+    parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR', './model'))
+
+    args = parser.parse_args()
+    
+    print(args)
+
+    # if args.wdb_on:
+    #     wandb.init(
+    #         project="Mask image Classification Competition",
+    #         notes="mask, gender, age 각각의 single task 탐구",
+    #         config={
+    #             "img_size": args.resize,
+    #             "loss": args.criterion,
+    #             "learning_rate": args.lr,
+    #             "architecture": args.model,
+    #             "dataset": args.dataset,
+    #             "augmentation": args.augmentation,
+    #             "epochs": args.epochs,
+    #             "optimizer": args.optimizer,
+    #             "batch_size": args.batch_size
+    #         }
+    #     )
+    #     wandb.run.name = args.name
+    #     wandb.run.save()
+        
+
+    data_dir = args.data_dir
+    model_dir = args.model_dir
+
+    train(data_dir, model_dir, args)
